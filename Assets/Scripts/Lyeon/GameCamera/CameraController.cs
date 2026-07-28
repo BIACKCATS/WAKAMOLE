9dedad282c6208145186be9ad21f8036a3b2f575
Lyeon <devlyeon@gmail.com>
2026-07-28 09:06:24 +0900

    add camera move

--- Assets/Scripts/Lyeon/GameCamera/CameraController.cs
+++ Assets/Scripts/Lyeon/GameCamera/CameraController.cs
@@ -5,32 +5,27 @@ namespace Wakamole.Lyeon.GameCamera
 {
     public class CameraController : MonoBehaviour
     {
-        // 미작동으로 인해...
-        /*
-        [Header("Components")]
-        [Tooltip("카메라입니다.")]
-        [SerializeField] private Camera cam;
-
         [Header("Information")]
         [Tooltip("카메라의 이동 속도입니다.")]
         [SerializeField] private float moveSpeed = 4.0f;
-        [Tooltip("카메라가 위치 보정을 시작할 마우스와의 거리입니다.")]
-        [SerializeField] private Vector2 mouseDistanceLimit;
-        [Tooltip("카메라가 이동 가능한 현재 위치 기준 범위입니다.")]
-        [SerializeField] private Vector2 moveLimit;
+        [Tooltip("카메라가 움직이지 않는 영역의 최대값입니다.")]
+        [SerializeField] private Vector2 mouseMaxDistance;
+        [Tooltip("카메라가 움직이지 않는 영역의 최솟값입니다.")]
+        [SerializeField] private Vector2 mouseMinDistance;
+        [Tooltip("카메라가 이동 가능한 최대 좌표입니다.")]
+        [SerializeField] private Vector2 moveMaxLimit;
+        [Tooltip("카메라가 이동 가능한 최소 좌표입니다.")]
+        [SerializeField] private Vector2 moveMinLimit;
         [Tooltip("바닥으로 감지할 Layer입니다.")]
         [SerializeField] private LayerMask groundLayer;
 
         private Ray mouseRay;
-        private Vector2 currentMouse = Vector2.zero;
+        private Vector2 currentMouse = Vector2.zero; // 화면 상의 마우스 위치
         private Vector3 mousePosition = Vector3.zero, initPosition = Vector3.zero, targetPosition = Vector3.zero;
 
         private void Awake()
         {
             targetPosition = initPosition = transform.position;
-            
-            mouseDistanceLimit.x += initPosition.x;
-            mouseDistanceLimit.y += initPosition.z;
         }
 
         private void Update()
@@ -40,27 +35,24 @@ namespace Wakamole.Lyeon.GameCamera
             mouseRay = Camera.main.ScreenPointToRay(currentMouse);
 
             if (Physics.Raycast(mouseRay, out RaycastHit hit, Mathf.Infinity, groundLayer))
-            {
                 mousePosition = hit.point;
-                Debug.Log(mousePosition);
-            }
         }
 
         private void LateUpdate()
         {
-            // 위치 보정
-            if (mousePosition.x > mouseDistanceLimit.x) targetPosition.x += mousePosition.x - mouseDistanceLimit.x;
-            else if (mousePosition.x < -mouseDistanceLimit.x) targetPosition.x -= mousePosition.x + mouseDistanceLimit.x;
-            if (mousePosition.z > mouseDistanceLimit.y) targetPosition.z += mousePosition.z - mouseDistanceLimit.y;
-            else if (mousePosition.z < -mouseDistanceLimit.y) targetPosition.z -= mousePosition.z + mouseDistanceLimit.y;
+            if (mousePosition.x > mouseMaxDistance.x) targetPosition.x = initPosition.x - mouseMaxDistance.x + mousePosition.x;
+            else if (mousePosition.x < mouseMinDistance.x) targetPosition.x = initPosition.x - mouseMinDistance.x + mousePosition.x;
+            else targetPosition.x = initPosition.x;
+            if (mousePosition.z > mouseMaxDistance.y) targetPosition.z = initPosition.z - mouseMaxDistance.y + mousePosition.z;
+            else if (mousePosition.z < mouseMinDistance.y) targetPosition.z = initPosition.z - mouseMinDistance.y + mousePosition.z;
+            else targetPosition.z = initPosition.z;
 
-            // 이동 범위 보정
-            if (targetPosition.x < initPosition.x - moveLimit.x) targetPosition.x = initPosition.x - moveLimit.x;
-            else if (targetPosition.x > initPosition.x + moveLimit.x) targetPosition.x = initPosition.x + moveLimit.x;
-            if (targetPosition.z < initPosition.z - moveLimit.y) targetPosition.z = initPosition.z - moveLimit.y;
-            else if (targetPosition.z > initPosition.z + moveLimit.y) targetPosition.z = initPosition.z + moveLimit.y;
+            if (targetPosition.x > moveMaxLimit.x) targetPosition.x = moveMaxLimit.x;
+            else if (targetPosition.x < moveMinLimit.x) targetPosition.x = moveMinLimit.x;
+            if (targetPosition.z > moveMaxLimit.y) targetPosition.z = moveMaxLimit.y;
+            else if (targetPosition.z < moveMinLimit.y) targetPosition.z = moveMinLimit.y;
 
             transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-        } */
+        }
     }
 }
