af68868267003b6d445ed0ab8f48f776eb17d055
Lyeon <devlyeon@gmail.com>
2026-08-02 17:32:48 +0900

    fix mole score, show time, hp applying mechanism

--- Assets/Scripts/Lyeon/Entity/Mole/Mole.cs
+++ Assets/Scripts/Lyeon/Entity/Mole/Mole.cs
@@ -191,21 +191,24 @@ namespace Wakamole.Lyeon.Entity
             if (!fixedTime)
             {
                 fixedTime = moleData.isFixedTime;
-                if (fixedTime) showTime = moleData.showTime;
+
+                if (fixedTime || showTime < moleData.showTime) showTime = moleData.showTime;
                 else showTime += moleData.showTime;
             }
 
             if (!fixedScore)
             {
                 fixedScore = moleData.isFixedScore;
-                if (fixedScore) score = moleData.score;
+
+                if (fixedScore || score < moleData.score) score = moleData.score;
                 else score += moleData.score;
             }
 
             if (!fixedHp)
             {
                 fixedHp = moleData.isFixedHp;
-                if (fixedHp)
+
+                if (fixedHp || maxHp < moleData.hp)
                 {
                     maxHp = moleData.hp;
                     Hp = moleData.hp;
