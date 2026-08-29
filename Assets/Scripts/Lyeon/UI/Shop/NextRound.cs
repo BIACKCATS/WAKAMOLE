2ed6731b19fbbd014b8638b075b2527bf928aa8d
Lyeon <devlyeon@gmail.com>
2026-08-29 11:54:02 +0900

    fix bugs

--- Assets/Scripts/Lyeon/UI/Shop/NextRound.cs
+++ Assets/Scripts/Lyeon/UI/Shop/NextRound.cs
@@ -15,10 +15,12 @@ namespace Wakamole.Lyeon.UI.Shop
         public void OnPointerDown(PointerEventData eventData)
         {
             image.sprite = clickImage;
+            image.color = Color.gray;
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
+            image.color = Color.white;
             GameManager.Current.Audio.PlaySfx("Button");
             GameManager.Current.StageId++;
             SceneManager.LoadScene(sceneName);
